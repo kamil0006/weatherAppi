676c5e0967c2f0f2a2ab38d330f3ccab6e3523cd
Kamil <kamil@kamil-linux.home>
2023-03-11 17:44:03 +0100

    adding temp and hum

--- js/main.js
+++ js/main.js
@@ -12,3 +12,19 @@ const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
 const API_KEY = '&appid=6a5ef895533345a527b89941ec25b0'
 const API_UNITS = '&units=metric'
 
+const getWeather = () => {
+    const city = input.value || 'London'
+    const URL = API_LINK + city + API_KEY + API_UNITS
+
+    axios.get(URL).then(res => {
+        const temp = res.data.main.temp
+        const hum = res.data.main.humidity
+
+        cityName.textContent = res.data.name
+
+        temperature.textContent = Math.floor(temp) + '°C'
+        humidity.textContent = hum + '%'
+    })
+}
+
+getWeather()
