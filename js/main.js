9447890aa8b22da68d8ae11b0fea091dfe46916c
Kamil <kamil@kamil-linux.home>
2023-03-11 17:07:10 +0100

    api link, itp

--- js/main.js
+++ js/main.js
@@ -6,3 +6,9 @@ const photo = document.querySelector('.photo')
 const weather = document.querySelector('.weather')
 const temperature = document.querySelector('.temperature')
 const humidity = document.querySelector('.humidity')
+
+
+const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
+const API_KEY = '&appid=6a5ef895533345a527b89941ec25b0'
+const API_UNITS = '&units=metric'
+
