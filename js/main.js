26446041b70383f0f0e1d654e9dcca2226cc312c
Kamil <kamil@kamil-linux.home>
2023-03-11 18:24:14 +0100

    if else, click on Enter

--- js/main.js
+++ js/main.js
@@ -7,24 +7,56 @@ const weather = document.querySelector('.weather')
 const temperature = document.querySelector('.temperature')
 const humidity = document.querySelector('.humidity')
 
-
 const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
 const API_KEY = '&appid=6a5ef895533345a527b89941ec25b0'
 const API_UNITS = '&units=metric'
 
 const getWeather = () => {
-    const city = input.value || 'London'
-    const URL = API_LINK + city + API_KEY + API_UNITS
+	const city = input.value || 'London'
+	const URL = API_LINK + city + API_KEY + API_UNITS
 
-    axios.get(URL).then(res => {
-        const temp = res.data.main.temp
-        const hum = res.data.main.humidity
+	axios
+		.get(URL)
+		.then(res => {
+			const temp = res.data.main.temp
+			const hum = res.data.main.humidity
+			const status = Object.assign({}, ...res.data.weather)
 
-        cityName.textContent = res.data.name
+			cityName.textContent = res.data.name
+			temperature.textContent = Math.floor(temp) + '°C'
+			humidity.textContent = hum + '%'
+			weather.textContent = status.main
 
-        temperature.textContent = Math.floor(temp) + '°C'
-        humidity.textContent = hum + '%'
-    })
+            warning.textContent = ''
+            input.value = ''
+
+			if (status.id >= 200 && status.id < 300) {
+				photo.setAttribute('src', './img/thunderstorm.png')
+			} else if (status.id >= 300 && status.id < 400) {
+				photo.setAttribute('src', './img/drizzle.png')
+			} else if (status.id >= 500 && status.id < 600) {
+				photo.setAttribute('src', './img/rain.png')
+			} else if (status.id >= 600 && status.id < 700) {
+				photo.setAttribute('src', './img/ice.png')
+			} else if (status.id >= 700 && status.id < 800) {
+				photo.setAttribute('src', './img/fog.png')
+			} else if (status.id === 800) {
+				photo.setAttribute('src', './img/sun.png')
+			} else if (status.id > 800 && status.id < 900) {
+				photo.setAttribute('scr', './img/cloud.png')
+			} else {
+				photo.setAttribute('src', './img/unkonow.png')
+			}
+		})
+		.catch(() => warning.textContent = 'Wpisz poprawną nazwę miasta!')
 }
 
+const enterCheck = e => {
+    if (e.key === 'Enter') {
+        getWeather()
+    }
+}
+input.addEventListener('keyup', enterCheck)
+button.addEventListener('click', getWeather)
 getWeather()
+
